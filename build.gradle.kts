922bb8fff370c309f35b690772fc68e473dbd51e
Alwyn974 <alwyn974@gmail.com>
2025-05-04 16:05:43 +0400

    feat(build): initialize Gradle build system with Kotlin JVM, plugins, and dependencies
    - Added Gradle configuration for Kotlin JVM project
    - Included Shadow, Serialization, and Maven Publish plugins
    - Configured repositories, dependencies, and Java toolchain
    - Added Gradle wrapper files
    - Set up publishing to GitHub Packages
    - Included default project structure and ignore files

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+import java.net.URI
+
+plugins {
+    kotlin("jvm") version "2.1.20"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    kotlin("plugin.serialization") version "2.1.20"
+    id("maven-publish")
+}
+
+group = "re.neotamia.schema"
+// x-release-please-start-version
+version = "1.0.0-SNAPSHOT"
+// x-release-please-end
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+    maven {
+        name = "jitpack"
+        url = uri("https://jitpack.io")
+    }
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.build {
+    dependsOn(tasks.shadowJar)
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile>().configureEach {
+    jvmTargetValidationMode.set(org.jetbrains.kotlin.gradle.dsl.jvm.JvmTargetValidationMode.WARNING)
+}
+
+publishing {
+    repositories {
+        maven {
+            name = "GitHubPackages"
+            url = URI("https://maven.pkg.github.com/NeoTamia/${project.name}")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+    }
+}
